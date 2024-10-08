be246fe8b11b825c0d2646d48dc1ce8ea551400f
Yaroslav Bochkov <cruelty86@yandex.ru>
2024-10-08 14:31:32 +0300

    add submodels

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, Tag, RecipeIngredient, RecipeTag
 
-# Register your models here.
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1  # Количество пустых форм для добавления
+
+class RecipeTagInline(admin.TabularInline):
+    model = RecipeTag
+    extra = 1  # Количество пустых форм для добавления
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline, RecipeTagInline]
+    list_display = ('title', 'author', 'created_at')
+    search_fields = ('title',)
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'amount')
+
+@admin.register(RecipeTag)
+class RecipeTagAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'tag')
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -14,17 +14,18 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
-    
-    class Ingredient(models.Model):
-        name = models.CharField(max_length=200, verbose_name='Название Ингредиента')
-        measurement_unit = models.CharField(max_length=50, verbose_name='Единица измерения')
 
-        class Meta:
-            verbose_name = 'Ингредиент'
-            verbose_name_plural = 'Ингредиенты'
 
-        def __str__(self):
-            return self.name
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название Ингредиента')
+    measurement_unit = models.CharField(max_length=50, verbose_name='Единица измерения')
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
@@ -36,3 +37,22 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, verbose_name='Рецепт')
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, verbose_name='Ингредиент')
+    amount = models.PositiveIntegerField(verbose_name='Количество')
+
+    class Meta:
+        verbose_name = 'Ингредиент рецепта'
+        verbose_name_plural = 'Ингредиенты рецепта'
+
+
+class RecipeTag(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, verbose_name='Рецепт')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='Тег')
+
+    class Meta:
+        verbose_name = 'Тег рецепта'
+        verbose_name_plural = 'Теги рецепта'
