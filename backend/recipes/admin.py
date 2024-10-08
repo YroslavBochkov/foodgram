af196c6f03cd1319c3bd01f9968a7bf684d25417
Yaroslav Bochkov <cruelty86@yandex.ru>
2024-10-08 19:27:22 +0300

    add requirements from pip freeze

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,18 +1,14 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, Tag, RecipeIngredient, RecipeTag
+from .models import Recipe, Ingredient, Tag, RecipeIngredient
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
     extra = 1  # Количество пустых форм для добавления
 
-class RecipeTagInline(admin.TabularInline):
-    model = RecipeTag
-    extra = 1  # Количество пустых форм для добавления
-
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    inlines = [RecipeIngredientInline, RecipeTagInline]
-    list_display = ('title', 'author', 'created_at')
+    inlines = [RecipeIngredientInline]
+    list_display = ('title', 'author', 'cooking_time')
     search_fields = ('title',)
 
 @admin.register(Ingredient)
@@ -22,13 +18,5 @@ class IngredientAdmin(admin.ModelAdmin):
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'slug')
     search_fields = ('name',)
-
-@admin.register(RecipeIngredient)
-class RecipeIngredientAdmin(admin.ModelAdmin):
-    list_display = ('recipe', 'ingredient', 'amount')
-
-@admin.register(RecipeTag)
-class RecipeTagAdmin(admin.ModelAdmin):
-    list_display = ('recipe', 'tag')
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,24 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-class Recipe(models.Model):
-    title = models.CharField(max_length=200, verbose_name='Название')
-    description = models.TextField(verbose_name='Описание')
-    instructions = models.TextField(verbose_name='Инструкции')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes', verbose_name='Автор')
-
-    class Meta:
-        verbose_name = 'Рецепт'
-        verbose_name_plural = 'Рецепты'
-
-    def __str__(self):
-        return self.title
+from slugify import slugify  # Убедитесь, что у вас установлен пакет 
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=200, verbose_name='Название Ингредиента')
-    measurement_unit = models.CharField(max_length=50, verbose_name='Единица измерения')
+    name = models.CharField(
+        max_length=250,
+        verbose_name='Название ингредиента',
+    )
+    measurement_unit = models.CharField(
+        max_length=100,
+        verbose_name='Единица измерения',
+    )
 
     class Meta:
         verbose_name = 'Ингредиент'
@@ -29,14 +22,48 @@ class Ingredient(models.Model):
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Название тега')
+    name = models.CharField(max_length=50, verbose_name='Название тега', unique=True)
+    slug = models.SlugField(max_length=50, unique=True, verbose_name='Slug')
 
     class Meta:
         verbose_name = 'Тег'
         verbose_name_plural = 'Теги'
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)  # Генерация slug на основе названия
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Рецепт"""
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор',
+    )
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название',
+    )
+    image = models.ImageField(upload_to='recipes/images/', verbose_name='Картинка', blank=True, null=True)
+    description = models.TextField(verbose_name='Описание')
+    ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredient', verbose_name='Ингредиенты')
+    tags = models.ManyToManyField(Tag, verbose_name='Теги')
+    cooking_time = models.PositiveIntegerField(default=0, verbose_name='Время приготовления (минуты)')
+
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.title
 
 
 class RecipeIngredient(models.Model):
@@ -47,12 +74,3 @@ class RecipeIngredient(models.Model):
     class Meta:
         verbose_name = 'Ингредиент рецепта'
         verbose_name_plural = 'Ингредиенты рецепта'
-
-
-class RecipeTag(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, verbose_name='Рецепт')
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name='Тег')
-
-    class Meta:
-        verbose_name = 'Тег рецепта'
-        verbose_name_plural = 'Теги рецепта'
