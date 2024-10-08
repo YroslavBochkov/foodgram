6a43d645927b71dddbf26576aa982a16d72337c3
Yaroslav Bochkov <cruelty86@yandex.ru>
2024-10-08 14:13:35 +0300

    add standart vodels

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,1 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    instructions = models.TextField(verbose_name='Инструкции')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes', verbose_name='Автор')
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.title
+    
+    class Ingredient(models.Model):
+        name = models.CharField(max_length=200, verbose_name='Название Ингредиента')
+        measurement_unit = models.CharField(max_length=50, verbose_name='Единица измерения')
+
+        class Meta:
+            verbose_name = 'Ингредиент'
+            verbose_name_plural = 'Ингредиенты'
+
+        def __str__(self):
+            return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название тега')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
